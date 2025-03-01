3e1730c890f0b0eea25dae29b81d12128ed38095
jayalwayscodes <johnsonjardocs@gmail.com>
2025-03-01 06:23:10 +0100

    modified deploy script

--- deploy_favorites.py
+++ deploy_favorites.py
@@ -1,5 +1,11 @@
+from vyper import compile_code
+
 def main():
-    print("Hello from deploy_favorites.py")
+    print("Let's read in the vyper code and deploy it!")
+    with open("favorites.vy") as favorites_file:
+        favorites_code = favorites_file.read()
+        compilation_details = compile_code(favorites_code, output_formats=["bytecode"])
+        #print(compilation_details)
     
 
 if __name__ == "__main__":
