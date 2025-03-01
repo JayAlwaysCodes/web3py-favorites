2ab69d644ba1b16a92d77c0f7fe59eb1a0c4727a
jayalwayscodes <johnsonjardocs@gmail.com>
2025-03-01 22:04:24 +0100

    modified

--- deploy_favorites.py
+++ deploy_favorites.py
@@ -1,11 +1,23 @@
 from vyper import compile_code
+from web3 import Web3
 
 def main():
     print("Let's read in the vyper code and deploy it!")
     with open("favorites.vy") as favorites_file:
         favorites_code = favorites_file.read()
-        compilation_details = compile_code(favorites_code, output_formats=["bytecode"])
-        #print(compilation_details)
+        compilation_details = compile_code(favorites_code, output_formats=["bytecode", "abi"])
+        print(compilation_details)
+
+    w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:8545"))
+
+    favorites_contract = w3.eth.contract(bytecode=compilation_details["bytecode"], abi=compilation_details["abi"])
+    print("Building the transaction...")
+    transaction = favorites_contract.constructor().build_transaction()
+    # transaction = {
+
+    # }
+    print("Signing the transaction...")
+    print("transaction: ", transaction)
     
 
 if __name__ == "__main__":
